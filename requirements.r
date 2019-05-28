6c3d64e42aaeca36d714861a3938f1210aeb1f7a
Richard Cotton <richierocks@gmail.com>
2019-05-28 15:19:25 -0400

    devtools -> remotes, deal with archived pkg

--- requirements.r
+++ requirements.r
@@ -1,3 +1,7 @@
-devtools::install_version("rdatamarket", "0.6.5")
-devtools::install_version("plyr", "1.8.4")
-devtools::install_version("googleVis", "0.6.1")
+library(remotes)
+# rdatamarket has been archived
+download.file("https://cran.r-project.org/src/contrib/Archive/rdatamarket/rdatamarket_0.6.5.tar.gz", "rdatamarket_0.6.5.tar.gz")
+install.packages("rdatamarket_0.6.5.tar.gz", repos = NULL, type = "source")
+# install_version("rdatamarket", "0.6.5")
+install_version("plyr", "1.8.4")
+install_version("googleVis", "0.6.4")
