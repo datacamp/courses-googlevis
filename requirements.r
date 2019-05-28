7cb9239e03c11463384639b6629a9164360ee256
Richard Cotton <richierocks@gmail.com>
2019-05-28 16:38:39 -0400

    revert to install_version
    It works on archived pkgs

--- requirements.r
+++ requirements.r
@@ -1,7 +1,4 @@
 library(remotes)
-# rdatamarket has been archived
-download.file("https://cloud.r-project.org/src/contrib/Archive/rdatamarket/rdatamarket_0.6.5.tar.gz", "rdatamarket_0.6.5.tar.gz")
-install.packages("rdatamarket_0.6.5.tar.gz", "/usr/local/lib/R/site-library", repos = NULL, type = "source")
-# install_version("rdatamarket", "0.6.5")
+install_version("rdatamarket", "0.6.5")
 install_version("plyr", "1.8.4")
 install_version("googleVis", "0.6.4")
